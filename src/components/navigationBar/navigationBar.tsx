660344bbf6ed93fb2ebc904dad2746371f6950a0
Llavrov <51132146+Llavrov@users.noreply.github.com>
2023-10-14 01:31:44 +0300

    added info page

--- src/components/navigationBar/navigationBar.tsx
+++ src/components/navigationBar/navigationBar.tsx
@@ -4,12 +4,22 @@ import mapIcon from './map.svg';
 import blueInfoIcon from './info-blue.svg';
 import blueMapIcon from './map-blue.svg';
 
-export const NavigationBar = () => {
+type TNavigationBar = {
+    onClickInfo: () => void;
+    onClickMap: () => void;
+}
+
+export const NavigationBar = ({ onClickInfo, onClickMap }: TNavigationBar) => {
     const [activeTab, setActiveTab] = useState('map');
+
+    function handleClickIcon(callBack: () => void, icon: string) {
+        setActiveTab(icon);
+        callBack();
+    }
 
     return (
         <div className="flex justify-around rounded-10 px-5 w-[200px] h-[60px] bg-black">
-            <div className="flex justify-center" onClick={() => setActiveTab('info')}>
+            <div className="flex justify-center" onClick={() => handleClickIcon(onClickInfo, 'info')}>
                 {
                     activeTab === 'info' ? (
                         <img className="cursor-pointer" src={blueInfoIcon} alt={'active info icon'} width={28} height={28}/>
@@ -18,7 +28,7 @@ export const NavigationBar = () => {
                     )
                 }
             </div>
-            <div className="flex justify-center" onClick={() => setActiveTab('map')}>
+            <div className="flex justify-center" onClick={() => handleClickIcon(onClickMap, 'map')}>
                 {
                     activeTab === 'map' ? (
                         <img className="cursor-pointer" src={blueMapIcon} alt={'active map icon'} width={28} height={28}/>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,12 +3,24 @@ import {NavigationBar} from "../../components/navigationBar/navigationBar";
 import { Map } from '../../components/map';
 
 export const HomePage: React.FC = () => {
+    const [isOpen, setOpen] = useState(false);
+    const [showMap, setShowMap] = useState(true);
+
+
     return (
         <>
             <div className="absolute z-10 bottom-10 left-[calc(50vw-100px)]">
-                <NavigationBar />
+                <NavigationBar onClickInfo={() => setShowMap(false)} onClickMap={() => setShowMap(true)} />
             </div>
-            <Map />
+            {
+                showMap ? (
+                    <Map />
+                ) : (
+                    <div className="w-screen h-screen bg-white">
+
+                    </div>
+                )
+            }
         </>
     );
 }
